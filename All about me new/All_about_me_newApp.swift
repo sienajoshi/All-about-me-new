464f126972f70a83530ed5b1cfb7ac59de0e9ca2
Siena <sienajoshi@gmail.com>
2023-06-26 15:39:48 -0400

    Initial Commit

---
+++ All about me new/All_about_me_newApp.swift
@@ -0,0 +1,17 @@
+//
+//  All_about_me_newApp.swift
+//  All about me new
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct All_about_me_newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ All about me new/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  All about me new
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+//    @State private var name = "all about me!"
+//    @State private var textTitle = "Fun facts about me"
+    @State private var showDetails = false
+    
+    var body: some View {
+        VStack {
+            ZStack {
+
+                Image("chapelhillsunset")
+                Text("All about me")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(/*@START_MENU_TOKEN@*/[.leading, .bottom, .trailing], 500.0/*@END_MENU_TOKEN@*/)
+                Button("CLICK ME!!") {
+                            self.showDetails = true
+                        }
+                .buttonStyle(OutlineButtonStyle())
+                    VStack {
+                            
+                    }
+                                }
+        }//end of vstack
+        
+            
+        }//end of body
+    }//end of struct
+struct OutlineButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(lineWidth: 2)
+                    .background(Color.cyan)
+                //                .padding()
+                //                .position(x:200, y:100)
+                //
+            )
+    
+    }
+
+            }//end of ButtonStyle
+
+
+//end of struct
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
